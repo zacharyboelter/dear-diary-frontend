a5d7753947132ebe8358ec9e5757db6072d9516d
Dh529885 <89321290+Dh529885@users.noreply.github.com>
2021-11-08 14:35:12 -0500

    working Create,Update,and Delete routes

---
+++ src/components/Startpage.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import './comp-styles.css';
+
+function Startpage() {
+    return (
+        <div className="wrapper">
+            <h3>Welcome to...</h3>
+            <div className="main">
+                <span class="letter"></span>
+                <span>H</span>
+                <span>∙</span>
+                <span>D</span>
+                <span>E</span>
+                <span>A</span>
+                <span>R</span>
+                <span>∙</span>
+                <span>D</span>
+                <span>I</span>
+                <span>A</span>
+                <span>R</span>
+                <span>Y</span>
+            </div>
+            <button 
+            onClick={}
+            className='button'>Tell Me About Your Day ➔
+            </button>
+           
+          
+            <p>a diary for developers</p>
+        </div>
+    )
+}
+
+export default Startpage
